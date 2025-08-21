35b266d3fa37aa63374378f1549e9a0b7e42bbe2
FlorinPtr <54172753+FlorinPtr@users.noreply.github.com>
2025-08-21 07:39:38 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       />
 
 
-        {!searchDone && (
+        {true && (
           <div className="mt-4 text-center">
             <p>
               {showFullText
